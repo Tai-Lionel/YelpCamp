873ab1eacd314a4ebed7b2aca0ee6ac194125aee
Tai-Lionel <91473490+Tai-Lionel@users.noreply.github.com>
2023-06-29 17:04:22 +0700

    Update index.js

--- index.js
+++ index.js
@@ -156,6 +156,7 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render('error', { err })
 })
 
-app.listen('3000', () => {
+const port = process.env.PORT || 3000
+app.listen(port, () => {
     console.log('Port 3000 has been activated')
 })
